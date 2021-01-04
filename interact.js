ae4821ac89886a981e382ceeb3164e0318a570cc
Vasu Verma <vasverma@amazon.com>
2021-01-05 01:08:42 +0530

    Made data table dynamically populated from JSON

--- interact.js
+++ interact.js
@@ -1,5 +1,9 @@
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+var pointsTable = JSON.parse(data);
+var myTeams = JSON.parse(teams);
+var scores = [];
+
+for(var i=0; i<myTeams.length; i++) {
+  scores.push(scorePerTeam(myTeams[i]));
 }
 
 // Close the dropdown if the user clicks outside of it
@@ -27,9 +31,28 @@ window.onclick = function(event) {
   }
 }
 
+function scorePerTeam(team) {
+  var players1 = team.team1;
+  var players2 = team.team2;
+  var score1 = 0;
+  var score2 = 0;
 
-function myFunction2() {
-  document.getElementById("myDropdown2").classList.toggle("show");
+  for(var i=0; i < players1.length; i++ ) {
+    score1 += pointsTable[players1[i]]
+  }
+
+  for(var i=0; i < players2.length; i++ ) {
+    score2 += pointsTable[players2[i]]
+  }
+
+  score1 += pointsTable[team.team1Captain]
+  score2 += pointsTable[team.team2Captain]
+  score1 += 0.5*pointsTable[team.team1ViceCaptain]
+  score2 += 0.5*pointsTable[team.team2ViceCaptain]
+  var totalScore = score1+score2;
+
+  return {score1, score2, totalScore};
+
 }
 
 // Close the dropdown if the user clicks outside of it
