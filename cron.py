b34a7eef2f84ab679d0d19789bdbd003de10d360
nitin411 <herebynitin@gmail.com>
2021-01-14 13:38:02 +0000

    Updated scores

--- cron.py
+++ cron.py
@@ -1,7 +1,7 @@
 from crontab import CronTab
 
 cron = CronTab(user='cron')
-job = cron.new(command='python calculate_points.py')
-job.minute.every(1)
+job = cron.new(command='python3 calculate_points.py')
+job.minute.every(2)
 
 cron.write()
