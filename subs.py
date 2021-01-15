370a4e2d8e3247f0c4a8f75c16ed2a81ad6ab611
Rahul Manchanda <rmanchanda@expedia.com>
2021-01-15 20:50:34 +0530

    Subs.json

--- subs.py
+++ subs.py
@@ -1,6 +1,28 @@
 import json
+import pandas as pd
+
+class teams:
+  teamName = ""
+  playerName = ""
+  subIn = ""
+  subOut = ""
+  subDay = ""
+
+  def __init__(self, teamName, playerName, subIn, subOut, subDay):
+  	self.teamName = teamName
+  	self.playerName = playerName
+  	self.subIn = subIn
+  	self.subOut = subOut
+  	self.subDay = subDay
 
 if __name__ == "__main__":
   data = pd.read_excel("./data_files/Subs.xlsx")
-  dataFrame = pd.DataFrame(data, columns= ['Player Name', 'Sub In', 'Sub Out'])
-  
+  dataFrame = pd.DataFrame(data, columns= ['Team Name','Player Name', 'Sub In', 'Sub Out', 'Sub Day'])
+  subs = []
+  for index, row in dataFrame.iterrows():
+  	subs.append(teams(row['Team Name'], row['Player Name'], row['Sub In'], row['Sub Out'], row['Sub Day']))
+  with open("./data_files/teams/subs.json", "w") as file1:
+    file1.write("subs = '")
+    json_string = json.dumps([ob.__dict__ for ob in subs])
+    file1.write(json_string)
+    file1.write("'")
