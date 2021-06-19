900ac34969610c4773b76ea4f9429b63b2b1799a
Vasu Verma <vasverma@amazon.com>
2021-06-19 16:50:34 +0530

    Added NZ flag

--- substitute_automation.js
+++ substitute_automation.js
@@ -188,7 +188,7 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("countdown").innerHTML = "Substitution can only be made before the first ball of second innings is bowled";
+  document.getElementById("countdown").innerHTML = "Substitution will be made available after Day 1 is over";
 
   // If the count down is finished, write some text
   if (distance < 0) {
