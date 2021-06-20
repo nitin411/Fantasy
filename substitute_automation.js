00ce173d4ebecdec9255053a57e8c47fb20a41c3
Vasu Verma <vasverma@amazon.com>
2021-06-20 15:28:13 +0530

    Changes

--- substitute_automation.js
+++ substitute_automation.js
@@ -189,7 +189,7 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("countdown").innerHTML = "Substitution will be made available after Day 1 is over";
+  document.getElementById("countdown").innerHTML = "Substitutes made now will reflect after Day 3";
 
   // If the count down is finished, write some text
   if (distance < 0) {
